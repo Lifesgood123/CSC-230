196d0fabec5e8d42df54e8b992e9a807ba2cbb0a
Ellie Mae Galyean <tggalyea@uncg.edu>
2020-09-01 14:53:31 -0400

    Commenting

--- Assignment 1/Question02/src/Question02/Question02.java
+++ Assignment 1/Question02/src/Question02/Question02.java
@@ -4,34 +4,46 @@ import java.util.Arrays;
 import java.util.ArrayList;
 
 public class Question02 {
-  
   public static void main(String[] args){
+    // Take in single postional argument limit
     int limit = Integer.parseInt(args[0]);
-
+    int[] primes = getPrimes(limit);
+    for( int i : primes ){
+      System.out.print(i + ", ");
+    }
+  }
+  /**
+   * Calculates all primes to a non-inclusive limit
+   * @param limit - Upper limit of primes
+   * @return int[] of prime numbers 
+   */
+  public static int[] getPrimes(int limit){
+    ArrayList<Integer> primeList = new ArrayList<Integer>(0);
     boolean[] primes = new boolean[limit];
     Arrays.fill(primes, true);
     primes[0] = false;
     primes[1] = false;
-    System.out.println("HERE");
     for( int i = 2; i < primes.length; i++ ){
-      
       if(primes[i]){
-        int[] multiples = getmultiples(i, limit);
-        
+        ArrayList<Integer> multiples = getmultiples(i, limit);
         for (int j : multiples){
           primes[j] = false;
         }
-
       }
-
     }
     for(int i = primes.length - 1; i >= 0; i--){
-      if (primes[i]) System.out.print(i + ", ");
+      if (primes[i]) primeList.add(i);
     }
+    return primeList.stream().mapToInt(Integer::intValue).toArray();
   }
 
-
-  public static int[] getmultiples(int num, int limit){
+ /**
+  * Returns all multiples of a given number
+  * @param num Integer to return multiples of
+  * @param limit upper bound of multiples non-inclusive,
+  * @return ArrayList<Integer> of multiples of num
+  */
+  public static ArrayList<Integer> getmultiples(int num, int limit){
     ArrayList<Integer> multiples = new ArrayList<Integer>(0);
     for(int i = 2; i < limit; i++){
       int multiple = i * num;
@@ -40,6 +52,6 @@ public class Question02 {
       }
       multiples.add(multiple);
     }
-        return multiples.stream().mapToInt(Integer::intValue).toArray();
+        return multiples;
   }
 }
